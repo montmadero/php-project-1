4cc1aa78108b1f198322177a1291981a373c9ff1
irving <irvingfrias41@gmail.com>
2019-01-08 11:29:31 -0600

    feature/about-us: Created styles for section 4 to 6, at about us page.

---
+++ about-us.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include('inc/html.head.inc'); ?>
+    <title>HomePage | PHP Project 1</title>
+  </head>
+
+  <body>
+    <?php include('inc/header.inc'); ?>
+
+    <?php include('inc/about-us/section-4.inc'); ?>
+
+    <?php include('inc/footer.inc'); ?>
+  </body>
+</html>
