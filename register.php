0fcce79c469dc0e86aecfbeb6a038eea8b0d4c48
xenozalgo <rg-stfpop@hotmail.com>
2019-01-08 18:33:35 -0700

    feature/latest-news: Create news register pages

---
+++ register.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include('inc/html.head.inc');?>
+    <title>Good News</title>
+  </head>
+  <body>
+    <?php include('inc/header.inc');?>
+    <?php include('inc/news/news-register.inc');?>
+    <?php include('inc/footer.inc');?>
+  </body>
+</html>
