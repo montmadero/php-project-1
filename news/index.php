d7cd3195b186b0234f9a8c2314a2b657e247d6fb
Paul Hernandez <paul777gamer@gmail.com>
2019-01-09 13:41:17 -0700

    feature/fresh-news: Renamed news.php file.

---
+++ news/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include('../inc/html.head.inc'); ?>
+  </head>
+
+  <body>
+    <?php include('../inc/header.inc'); ?>
+
+    <?php include('../inc/news/00-news-latest-news.inc'); ?>
+    <?php include('../inc/news/01-news-fresh-news.inc'); ?>
+    <?php include('../inc/news/02-news-learn-the-secrets.inc'); ?>
+    <?php include('../inc/news/03-news-new-training-packages.inc'); ?>
+    <?php include('../inc/news/04-news-news-center.inc'); ?>
+
+    <?php include('../inc/footer.inc'); ?>
+  </body>
+</html>
