e1379330b9cad56a2f47f95b9a84340593f81ea7
normaabr <norma.abr23@gmail.com>
2019-01-09 16:37:51 -0700

    feature/header: Updated styles for header, mobile and desktop version.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
-var API;
+var mmenu1;
 
 $(document).ready(function() {
   $('#my-menu').mmenu();
-  API = $('#my-menu').data('mmenu');
+  mmenu1 = $('#my-menu').data('mmenu');
 });
 
 $(document).on('click', '#menu-button', function () {
-  API.open();
+  if ($('html').hasClass('mm-wrapper_opening')) {
+    mmenu1.close();
+  }
+  else {
+    mmenu1.open();
+  }
 });
 
-var API2;
+$(document).on('click', '.mm-wrapper_opening #header-mobile', function (e) {
+  mmenu1.close();
+});
+
+var mmenu2;
 
 $(document).ready(function() {
   $('#my-menu2').mmenu({
@@ -17,9 +26,9 @@ $(document).ready(function() {
           "position-right"
        ]
   });
-  API2 = $('#my-menu2').data('mmenu');
+  mmenu2 = $('#my-menu2').data('mmenu');
 });
 
 $(document).on('click', '#menu-button2', function () {
-  API2.open();
+  mmenu2.open();
 });
