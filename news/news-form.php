65af26ddbabf1c434ec131e803c3d6dccd1bf407
xenozalgo <rg-stfpop@hotmail.com>
2019-01-10 16:33:54 -0700

    feature/dinamic-news: Added styles for news item add page.

---
+++ news/news-form.php
@@ -0,0 +1,13 @@
+<?php $title = 'NEWS';?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php include('../inc/header-black.inc'); ?>
+    <title>Good News</title>
+  </head>
+  <body>
+    <?php include('../inc/header.inc'); ?>
+    <?php include('../inc/news/news-form.inc'); ?>
+    <?php include('../inc/footer.inc'); ?>
+  </body>
+</html>
