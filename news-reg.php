f0e22afe6c7481afb88a4a9100dc1d97af388ad0
xenozalgo <rg-stfpop@hotmail.com>
2019-01-10 12:01:33 -0700

    bug/news: Modified some news pages and routes names

--- news-reg.php
+++ news-reg.php
@@ -2,12 +2,12 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <?php include('inc/html.head.inc'); ?>
+    <?php include('inc/html.head.inc');?>
     <title>Good News</title>
   </head>
   <body>
-    <?php include('inc/header.inc'); ?>
-    <?php include('inc/news/news-form.inc'); ?>
-    <?php include('inc/footer.inc'); ?>
+    <?php include('inc/header.inc');?>
+    <?php include('inc/news/news-register.inc');?>
+    <?php include('inc/footer.inc');?>
   </body>
 </html>
